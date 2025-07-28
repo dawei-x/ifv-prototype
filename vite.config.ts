090cee086e660a63c2f5ee55116bee7bebcb9cca
dawei-x <dawei.x@wustl.edu>
2025-07-27 21:32:40 -0500

    configure gh deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const repoName = 'ifv-prototype'; 
+
 export default defineConfig({
+	base: `/${repoName}/`,
 	plugins: [sveltekit()]
 });
